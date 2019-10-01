b70dc445035d52e8c3ca502f987dde17553e17d0
jiwonchoi <jiwonchoi712@gmail.com>
2019-10-01 17:28:49 +0900

    Activity Lifecycle & onBackPressed

--- MyApplication5/app/src/main/java/com/example/myapplication5/MainActivity.java
+++ MyApplication5/app/src/main/java/com/example/myapplication5/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.myapplication5;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,13 +24,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==1234 && resultCode==RESULT_OK){
+            Log.d("",data.getStringExtra("from_second"));
+        }
+    }
+    int count=0;
+
+    @Override
+    public void onBackPressed() {
+        if(count==0) {
+            count++;
+            Toast.makeText(this, "한번 더 누르시면 종료됩니다", Toast.LENGTH_LONG).show();
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.tv_first:
                 Intent intent= new Intent(this, SecondActivity.class);
-                startActivity(intent);
+                //startActivity(intent); //그냥 보여주는 것
+                startActivityForResult(intent, 1234); //결과를 받기 위해 호출
                 break;
             case R.id.tv_second:
+                Intent intent2 = new Intent(Intent.ACTION_DIAL);
+                intent2.setData(Uri.parse("tel:01012341234"));
+                startActivity(intent2);
                 break;
 
         }
--- MyApplication5/app/src/main/java/com/example/myapplication5/SecondActivity.java
+++ MyApplication5/app/src/main/java/com/example/myapplication5/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        TextView tvGo=findViewById(R.id.tv_go);
+        tvGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("from_second", "뒤에서 왔어요!");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
